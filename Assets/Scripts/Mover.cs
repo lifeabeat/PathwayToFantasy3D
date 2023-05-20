7b84e298dc2d846e91e5073770357c22cdc2edb5
lifeabeat <tranphudat1@gmail.com>
2023-05-21 01:04:05 +0700

    [add] Player Movement - Done

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,10 +10,13 @@ public class Mover : MonoBehaviour
     private void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
+
+        UpdateAnimator();
+
     }
 
     private void MoveToCursor()
@@ -25,6 +28,16 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }    
-    }    
-        
+    }   
+    
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        // Change velocity from worldspace to localspace because when creating velocity, navmesh velocity store the velocity as variable in UpdateAnimator as global coordinate
+        // Animator just know Running forward or not. So use Inverse to tell Animator that Animator is moving forward 
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
 }
