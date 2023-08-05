2f2eb1fc9aa18bb55581de8e99dacc5af6eb11c3
lifeabeat <tranphudat1@gmail.com>
2023-08-05 21:19:21 +0700

    [add] add UI, S.O for Dialogue Editor

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    [CreateAssetMenu(fileName = "Dialogue", menuName = "RPG PathwayToFantasy/Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+
+        [SerializeField]
+        List<DialogueNode> nodes;
+
+#if UNITY_EDITOR
+        private void Awake()
+        {
+         if(nodes.Count == 0)
+        {
+          nodes.Add(new DialogueNode());
+        }
+        }
+#endif
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+
+        public DialogueNode GetRootNode()
+        {
+            return nodes[0];
+        }
+    }
+}
